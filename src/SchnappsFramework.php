efbac367cccef3319f4a1c38b142bad01203c374
Tom Gurney <=>
2024-01-29 08:23:04 +0000

    dev: adds gtm config option

--- src/SchnappsFramework.php
+++ src/SchnappsFramework.php
@@ -53,6 +53,18 @@ class SchnappsFramework
                 gtag("config", "' . Config::get('seo.analytics.ga') . '");
               </script>';
       }
+
+      if (Config::get('seo.tagmanager.gtm')) {
+        echo '<!-- Google Tag Manager -->
+              <link rel="dns-prefetch" href="https://www.googletagmanager.com">
+              <link rel="preload" href="https://www.googletagmanager.com/gtm.js?id=' . Config::get('seo.tagmanager.gtm') . '" as="script">
+              <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({"gtm.start":
+              new Date().getTime(),event:"gtm.js"});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!="dataLayer"?"&l="+l:"";j.async=true;j.src=
+              "https://www.googletagmanager.com/gtm.js?id="+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,"script","dataLayer","' . Config::get('seo.tagmanager.gtm') . '");</script>
+              <!-- End Google Tag Manager -->';
+      }
     });
   }
 
